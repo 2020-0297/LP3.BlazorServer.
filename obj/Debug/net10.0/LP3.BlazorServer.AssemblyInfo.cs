66eedf7cc942be5cc27b2c7b7139921279822220
melvin <2020-0297@uteco.ed.do>
2026-06-09 20:32:03 -0400

    Proyecto recuperado y funcionando

--- obj/Debug/net10.0/LP3.BlazorServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/LP3.BlazorServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LP3.BlazorServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca675ec5592c3aae5abc529f3719e4b40e2ca4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5556786c835709f8ab212dbd1abd28ea1d16905f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LP3.BlazorServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LP3.BlazorServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
